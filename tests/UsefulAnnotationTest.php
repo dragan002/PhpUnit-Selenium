eac92f58b35198cef84fe43d57113566cc49b8ca
dragan002 <draganvujic29@gmail.com>
2024-08-07 12:39:25 +0200

    Annotation Test

---
+++ tests/UsefulAnnotationTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class UsefulAnnotationTest extends TestCase {
+
+    private $value;
+
+    /** 
+     * @before
+    */
+    public function renBeforeEachTestMethod() {
+        $this->value++;
+    }
+
+    /**
+     * @after
+     */
+
+     public function runAfterEachTestMethod()
+     {
+        unset($this->value);
+     }
+
+    public function testAnnotation1()
+    {
+        $this->value++;
+
+        $expected = 2;
+
+        $result = $this->value;
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testAnnotation2() {
+        $this->value++;
+
+        $expected = 2;
+
+        $result = $this->value;
+
+        $this->assertEquals($expected, $result);
+    }
+
+}
