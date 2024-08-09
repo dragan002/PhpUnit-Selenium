f4cafd8ac8cf7dcf892a41432f214c02e0f2aa31
dragan002 <draganvujic29@gmail.com>
2024-08-09 21:27:22 +0200

    Usses nonymous classes to mock object

--- src/User.php
+++ src/User.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use Database;
+
 class User {
 
     protected $name;
+
+    protected $db;
 
     private $lastName;
 
@@ -12,10 +16,13 @@ class User {
     {
         $this->name = ucfirst($name);
         $this->lastName = ucfirst($lastName);
+        $this->db = new Database;
     }
 
     public function getFullName()
     {
+        $this->db->getEmailAndLastName();
         return $this->name . ' ' . $this->lastName;
     }
+
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,18 +1,46 @@
 <?php
 
-use App\User;
 use PHPUnit\Framework\TestCase;
+use App\User;  // Ensure User class is under the App namespace and correctly imported.
+
+class Database 
+{
+    public function getEmailAndLastName()
+    {
+        // Return a value for testable output.
+        return "Email: sample@example.com";
+    }
+}
 
 class UserTest extends TestCase
 {
     public function testValidUserName()
     {
-        $user = new class('donald', 'Trump') extends User
-        {
+        $user = new class('donald', 'Trump') extends User {
             public function getName() {
-                return $this->name;
+                return $this->name;  // Directly access the protected property via the subclass.
             }
         };
         $this->assertEquals('Donald', $user->getName());
+    }
+
+    public function testValidDataFormat()
+    {
+        $user = new User('donald', 'Trump');
+        $mockedDb = new class extends Database {
+            public function getEmailAndLastName() {
+                return "Mocked DB used";  // Return mock response.
+            }
+        };
+
+        $setUserClosure = function () use ($mockedDb) {
+            $this->db = $mockedDb;
+        };
+
+        // Bind the closure to the $user object and execute.
+        $setUserClosure = $setUserClosure->bindTo($user, get_class($user));
+        $setUserClosure();
+
+        $this->assertSame('Donald Trump', $user->getFullName());  // Ensure getFullName works as expected.
     }
 }
