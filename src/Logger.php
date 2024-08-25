9a27a1f7e1933e55f91c55b049e675a2b4614d4a
dragan002 <draganvujic29@gmail.com>
2024-08-25 10:06:29 +0200

    CodeCoverage completing

--- src/Logger.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Logger 
-{
-    public static function log($data)
-    {
-        echo $data . "logged";
-    }
-}
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -2,30 +2,129 @@
 
 use PHPUnit\Framework\TestCase;
 
-class CalculatorTest extends TestCase
+class CalculatorTest extends TestCase 
 {
     public $calculator;
 
     protected function setUp(): void
     {
-        $this->calculator = new Calculator();
+        $this->calculator = new Calculator;
     }
 
-    public function testAdd()
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testAdd($a, $b, $expected)
     {
-        $this->assertEquals(5, $this->calculator->add(2, 3));
+        $result = $this->calculator->add($a, $b);
+        $this->assertEquals($expected, $result);
     }
 
-    public function testSubtract()
+    public static function additionProvider()
     {
-        $this->assertEquals(22, $this->calculator->subtract(30, 8));
+        return [
+            'both positive' => [2, 3, 5],
+            'positive and negative' => [2, -3, -1],
+            'both negative' => [-2, -3, -5],
+            'zero and positive' => [0, 5, 5],
+            'zero and negative' => [0, -5, -5],
+        ];
     }
 
-    public function testMultiply() {
-        $this->assertEquals(20, $this->calculator->multiply(10, 2));
+    /** 
+    * @dataProvider subtractProvider
+    */
+
+    public function testSubtract($a, $b, $expected)
+    {
+        if($a < 0){
+            $this->expectException(InvalidArgumentException::class);
+            $this->expectExceptionMessage('First number must be positive');
+        }
+        $result = $this->calculator->subtract($a, $b);
+        $this->assertEquals($expected, $result);
     }
 
-    public function testDivide() {
-        $this->assertEquals(30, $this->calculator->divide(90, 3));
+    public static function subtractProvider()
+    {
+        return [
+            'positive and positive' => [5, 3, 2],
+            'positive and negative' => [5, -3, 8],
+            'negative and positive' => [-5, 3, -8],
+            'negative and negative' => [-5, -3, -2],
+            'zero and positive'     => [0, 5, -5],
+            'zero and negative'     => [0, -5, 5],
+        ];
+    }
+
+    public function testSubtractNegativeFirstNumber()
+    {
+    $this->expectException(InvalidArgumentException::class);
+    $this->expectExceptionMessage('First number must be positive');
+    $this->calculator->subtract(-5, 3);
+    }
+
+    /** 
+     * @dataProvider multiplyProvider 
+     */ 
+
+    public function testMultiply($a, $b, $expected) 
+    {
+        $result = $this->calculator->multiply($a, $b);
+        $this->assertEquals($expected, $result);
+    }
+
+    public static function multiplyProvider() {
+        return [
+            'positive and positive' => [2, 3, 6],
+            'positive and negative' => [2, -3, -6],
+            'negative and positive' => [-2, 3, -6],
+            'negative and negative' => [-2, -3, 6],
+            'zero and positive' => [0, 5, 0],
+            'zero and negative' => [0, -5, 0],
+        ];
+    }
+
+    /**
+     * @dataProvider  divideProvider
+     */
+
+    public function testDivide($a, $b, $expected) 
+    {
+        $result = $this->calculator->divide($a, $b);
+        $this->assertEquals($expected, $result);
+    }
+
+    public static function divideProvider()
+    {
+        return [
+            'positive and positive' => [90, 3, 30],
+            'positive and negative' => [90, -3, -30],
+            'negative and positive' => [-90, 3, -30],
+            'negative and negative' => [-90, -3, 30],
+        ];
+    }
+
+    /**
+     * @dataProvider  powerProvider
+     */
+    public function testPower($a, $expected) {
+        $result = $this->calculator->power($a);
+        $this->assertEquals($expected, $result);
+    }
+
+    public static function powerProvider() {
+        return [
+            'positive' => [2, 4],
+            'negative' => [-2, 4],
+            'zero' => [0, 0],
+            ];
+    }
+
+    public function testDivideByZero()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Dividing by 0 is always 0');
+        $this->calculator->divide(10, 0);  
     }
 }
